5a3742fd8133008af0dfa54fa46c8036f2b35aed
GrandpaBond <89019204+GrandpaBond@users.noreply.github.com>
2023-02-27 12:20:57 +0000

    morse using button-press

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+MORSE_TREE = "?ETIANMSURWDKGOHVF?L?PJBXCYZQ??54?3???2???????16???????7???8?91"
+morseIndex = 0
+command = "*"
 LOUD=200
 QUIET=100
-DOTMIN=100
-DASHMIN=300
-LETTERGAP=800
-isBusy=False
+DOT_MIN=100
+DASH_MIN=300
+LETTER_GAP=800
 isLoud=False
 mark=0
 gap=0
@@ -11,34 +13,66 @@ goLoud=0
 goQuiet=input.running_time()
 
 def listen():
-    global isBusy,isLoud,goLoud,goQuiet,mark,gap 
-    if isBusy: # building a letter 
-        if isLoud:
-            if input.sound_level()<QUIET:
-                isLoud=False
-                goQuiet=input.running_time()
-                mark=goQuiet-goLoud
-            # else beep lengthens
-        else:
-            if input.sound_level()>LOUD:
-                isLoud=True
-                goLoud=input.running_time()
-                gap=goLoud-goQuiet
-            # else gap lengthens
-    else: # between letters: wait for another 
+    global isLoud,goLoud,goQuiet,mark,gap 
+    if isLoud: # (beeping)
+        if input.sound_level()<QUIET:
+            isLoud=False # beep just ended
+            goQuiet=input.running_time()
+            mark=goQuiet-goLoud
+        # else beep lengthens
+    else: # (not beeping)
+        gap = input.running_time() - goQuiet # lengthen gap
         if input.sound_level()>LOUD:
-            isLoud=True
+            isLoud=True # new beep ends the gap
             goLoud=input.running_time()
-            isBusy=True 
+        # else gap lengthens
 
-def checkMorse(): 
-    pass 
+def monitor_button(): 
+# (same logic as listen(), but simulating sound with button_A)
+    global isLoud,goLoud,goQuiet,mark,gap
+    if isLoud: # (button down)
+        if not input.button_is_pressed(Button.A):
+            isLoud=False # button just released
+            goQuiet=input.running_time()
+            mark=goQuiet-goLoud
+        # else simulated "beep" lengthens
+    else: # (button up)
+        gap = input.running_time() - goQuiet # lengthen gap
+        if input.button_is_pressed(Button.A):
+            isLoud=True # new press ends the gap
+            goLoud=input.running_time()
+        # else gap lengthens
 
 
+def checkMorse():
+    global morseIndex, mark, command
+    if mark>0: # (just finished a beep)
+        if mark>DOT_MIN:
+            morseIndex += morseIndex
+        
+        if mark>DASH_MIN:
+            morseIndex += 1
+        if morseIndex > 63:
+            morseIndex = 0 # ignore a six-beep letter!
+        mark = 0
+    else: 
+        if gap>LETTER_GAP:
+            command = MORSE_TREE[morseIndex]
 
-
+def obey_command():
+    global command
+    basic.show_string(command)
+    basic.pause(3000)
+    command = "*"
 
 def on_forever():
-    listen()
+    global command
+    #listen()
+    monitor_button()
+    checkMorse()
+    if command == "*":
+        pass
+    else:
+        obey_command()
     
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,9 +1,11 @@
+let MORSE_TREE = "?ETIANMSURWDKGOHVF?L?PJBXCYZQ??54?3???2???????16???????7???8?91"
+let morseIndex = 0
+let command = "*"
 let LOUD = 200
 let QUIET = 100
-let DOTMIN = 100
-let DASHMIN = 300
-let LETTERGAP = 800
-let isBusy = false
+let DOT_MIN = 100
+let DASH_MIN = 300
+let LETTER_GAP = 800
 let isLoud = false
 let mark = 0
 let gap = 0
@@ -11,33 +13,99 @@ let goLoud = 0
 let goQuiet = input.runningTime()
 function listen() {
     
-    if (isBusy) {
-        //  building a letter 
-        if (isLoud) {
-            if (input.soundLevel() < QUIET) {
-                isLoud = false
-                goQuiet = input.runningTime()
-                mark = goQuiet - goLoud
-            }
-            
-        } else if (input.soundLevel() > LOUD) {
-            isLoud = true
-            goLoud = input.runningTime()
-            gap = goLoud - goQuiet
+    if (isLoud) {
+        //  (beeping)
+        if (input.soundLevel() < QUIET) {
+            isLoud = false
+            //  beep just ended
+            goQuiet = input.runningTime()
+            mark = goQuiet - goLoud
         }
         
-    } else if (input.soundLevel() > LOUD) {
-        isLoud = true
-        goLoud = input.runningTime()
-        isBusy = true
+    } else {
+        //  else beep lengthens
+        //  (not beeping)
+        gap = input.runningTime() - goQuiet
+        //  lengthen gap
+        if (input.soundLevel() > LOUD) {
+            isLoud = true
+            //  new beep ends the gap
+            goLoud = input.runningTime()
+        }
+        
     }
     
 }
 
-function checkMorse() {
+//  else gap lengthens
+function monitor_button() {
+    //  (same logic as listen(), but simulating sound with button_A)
+    
+    if (isLoud) {
+        //  (button down)
+        if (!input.buttonIsPressed(Button.A)) {
+            isLoud = false
+            //  button just released
+            goQuiet = input.runningTime()
+            mark = goQuiet - goLoud
+        }
+        
+    } else {
+        //  else simulated "beep" lengthens
+        //  (button up)
+        gap = input.runningTime() - goQuiet
+        //  lengthen gap
+        if (input.buttonIsPressed(Button.A)) {
+            isLoud = true
+            //  new press ends the gap
+            goLoud = input.runningTime()
+        }
+        
+    }
     
 }
 
+//  else gap lengthens
+function checkMorse() {
+    
+    if (mark > 0) {
+        //  (just finished a beep)
+        if (mark > DOT_MIN) {
+            morseIndex += morseIndex
+        }
+        
+        if (mark > DASH_MIN) {
+            morseIndex += 1
+        }
+        
+        if (morseIndex > 63) {
+            morseIndex = 0
+        }
+        
+        //  ignore a six-beep letter!
+        mark = 0
+    } else if (gap > LETTER_GAP) {
+        command = MORSE_TREE[morseIndex]
+    }
+    
+}
+
+function obey_command() {
+    
+    basic.showString(command)
+    basic.pause(3000)
+    command = "*"
+}
+
 basic.forever(function on_forever() {
-    listen()
+    
+    // listen()
+    monitor_button()
+    checkMorse()
+    if (command == "*") {
+        
+    } else {
+        obey_command()
+    }
+    
 })
