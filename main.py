edbc29f378078ba65b24fb997debde48cb9a9707
GrandpaBond <89019204+GrandpaBond@users.noreply.github.com>
2023-02-28 16:28:59 +0000

    added mode toggle on button B (use button A or microphone input)

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-MORSE_TREE = "?ETIANMSURWDKGOHVF?L?PJBXCYZQ??54?3???2???????16???????7???8?91"
-morseIndex = 0
-command = "?"
-LOUD=200
-QUIET=100
-DOT_MIN=20
+MORSE_TREE = "@?ETIANMSURWDKGOHVF?L?PJBXCYZQ??54?3???2???????16???????7???8?90"
+morseIndex = 1
+command = "*"
+LOUD=50
+QUIET=30
+DOT_MIN=30
 DASH_MIN=300
-LETTER_GAP=600
+LETTER_GAP=800
 isLoud=False
 mark=0
+beeps = -1
 gap=0
 goLoud=0
 goQuiet=input.running_time()
+mode = 0 #(button)
 
 def listen():
     global isLoud,goLoud,goQuiet,mark,gap 
@@ -30,55 +32,64 @@ def listen():
 def monitor_button(): 
 # (same logic as listen(), but simulating sound with button_A)
     global isLoud,goLoud,goQuiet,mark,gap
-    if isLoud: # (button down)
+    if isLoud: # (button was down)
         if not input.button_is_pressed(Button.A):
             isLoud=False # button just released
             goQuiet=input.running_time()
             mark=goQuiet-goLoud
         # else simulated "beep" lengthens
-    else: # (button up)
+    else: # (button was up)
         if input.button_is_pressed(Button.A):
             isLoud=True # new press ends the gap
-            goLoud=input.running_time()
-        gap = input.running_time() - goQuiet # lengthen gap
+            goLoud=input.running_time() 
+        # lengthen gap, even without new button press
+        gap = input.running_time() - goQuiet
             
 
 def checkMorse():
-    global morseIndex, mark, command
-    if mark>0: # (just finished a beep)      
-        if mark>DASH_MIN:
-            morseIndex += morseIndex + 1
-            # basic.show_icon(IconNames.YES)
-        elif mark>DOT_MIN:
-            morseIndex += morseIndex
-            # basic.show_icon(IconNames.NO)
-        # else:
-            # basic.show_icon(IconNames.SQUARE) 
-        if morseIndex > 63:
-            morseIndex = 0 # ignore any six-beep attempt!
-        mark = 0 # mark now dealt with
-    elif gap>LETTER_GAP:
+    global morseIndex, mark, command, beeps
+    if mark>0: # (just finished a beep)
+        if mark>DOT_MIN:
+            beeps += 1
+            if beeps == 5:  # ignore any six-beep attempt!      
+                morseIndex = 0
+                basic.clear_screen()
+                beeps = -1
+            else:            
+                morseIndex += morseIndex
+                led.plot(0, beeps)
+                if mark>DASH_MIN:   
+                    morseIndex += 1
+                    led.plot(1, beeps)
+                    led.plot(2, beeps)
+        mark = 0 # mark now dealt with (or too brief to count)
+    elif beeps >= 0 and gap > LETTER_GAP:
         command = MORSE_TREE[morseIndex]
-        morseIndex = 0
-
-def obey_command():
-    global command
-    for i in range(3):
-        basic.show_string(command)
-        basic.pause(100)
+        morseIndex = 1
+        beeps = -1
+        pause(500)  # allow time to show last beep
         basic.clear_screen()
-        basic.pause(100)
-    command = "?"
 
-def on_forever():
-    global command, mark
-    #listen()
-    monitor_button()
-    checkMorse()
-    if command == "?":
-        pass
-    else:
-        obey_command()
-    # basic.pause(10)
+def obey_command(todo):
+    basic.show_string(todo)
+    basic.pause(500)
+    basic.clear_screen()
+
+def on_button_pressed_b():
+    global mode
+    mode = 1-mode
     
+def on_forever():
+    global command
+    if mode == 0:
+        monitor_button()
+    else:
+        listen()
+    checkMorse()
+    if command != "*":
+        obey_command(command)
+        command = "*"
+    basic.pause(10)
+    
+input.on_button_pressed(Button.B, on_button_pressed_b)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,16 +1,19 @@
-let MORSE_TREE = "?ETIANMSURWDKGOHVF?L?PJBXCYZQ??54?3???2???????16???????7???8?91"
-let morseIndex = 0
-let command = "?"
-let LOUD = 200
-let QUIET = 100
-let DOT_MIN = 20
+let MORSE_TREE = "@?ETIANMSURWDKGOHVF?L?PJBXCYZQ??54?3???2???????16???????7???8?90"
+let morseIndex = 1
+let command = "*"
+let LOUD = 50
+let QUIET = 30
+let DOT_MIN = 30
 let DASH_MIN = 300
-let LETTER_GAP = 600
+let LETTER_GAP = 800
 let isLoud = false
 let mark = 0
+let beeps = -1
 let gap = 0
 let goLoud = 0
 let goQuiet = input.runningTime()
+let mode = 0
+// (button)
 function listen() {
     
     if (isLoud) {
@@ -42,7 +45,7 @@ function monitor_button() {
     //  (same logic as listen(), but simulating sound with button_A)
     
     if (isLoud) {
-        //  (button down)
+        //  (button was down)
         if (!input.buttonIsPressed(Button.A)) {
             isLoud = false
             //  button just released
@@ -52,68 +55,79 @@ function monitor_button() {
         
     } else {
         //  else simulated "beep" lengthens
-        //  (button up)
+        //  (button was up)
         if (input.buttonIsPressed(Button.A)) {
             isLoud = true
             //  new press ends the gap
             goLoud = input.runningTime()
         }
         
+        //  lengthen gap, even without new button press
         gap = input.runningTime() - goQuiet
     }
     
 }
 
-//  lengthen gap
 function checkMorse() {
     
     if (mark > 0) {
-        //  (just finished a beep)      
-        if (mark > DASH_MIN) {
-            morseIndex += morseIndex + 1
-        } else if (mark > DOT_MIN) {
-            //  basic.show_icon(IconNames.YES)
-            morseIndex += morseIndex
+        //  (just finished a beep)
+        if (mark > DOT_MIN) {
+            beeps += 1
+            if (beeps == 5) {
+                //  ignore any six-beep attempt!      
+                morseIndex = 0
+                basic.clearScreen()
+                beeps = -1
+            } else {
+                morseIndex += morseIndex
+                led.plot(0, beeps)
+                if (mark > DASH_MIN) {
+                    morseIndex += 1
+                    led.plot(1, beeps)
+                    led.plot(2, beeps)
+                }
+                
+            }
+            
         }
         
-        //  basic.show_icon(IconNames.NO)
-        //  else:
-        //  basic.show_icon(IconNames.SQUARE) 
-        if (morseIndex > 63) {
-            morseIndex = 0
-        }
-        
-        //  ignore any six-beep attempt!
         mark = 0
-    } else if (gap > LETTER_GAP) {
-        //  mark now dealt with
+    } else if (beeps >= 0 && gap > LETTER_GAP) {
+        //  mark now dealt with (or too brief to count)
         command = MORSE_TREE[morseIndex]
-        morseIndex = 0
-    }
-    
-}
-
-function obey_command() {
-    
-    for (let i = 0; i < 3; i++) {
-        basic.showString(command)
-        basic.pause(100)
+        morseIndex = 1
+        beeps = -1
+        pause(500)
+        //  allow time to show last beep
         basic.clearScreen()
-        basic.pause(100)
     }
-    command = "?"
+    
 }
 
-//  basic.pause(10)
+function obey_command(todo: string) {
+    basic.showString(todo)
+    basic.pause(500)
+    basic.clearScreen()
+}
+
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
+    
+    mode = 1 - mode
+})
 basic.forever(function on_forever() {
     
-    // listen()
-    monitor_button()
-    checkMorse()
-    if (command == "?") {
-        
+    if (mode == 0) {
+        monitor_button()
     } else {
-        obey_command()
+        listen()
     }
     
+    checkMorse()
+    if (command != "*") {
+        obey_command(command)
+        command = "*"
+    }
+    
+    basic.pause(10)
 })
